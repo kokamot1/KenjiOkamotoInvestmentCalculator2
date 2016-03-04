7c01a0faa7849b95e9ad04135bc1d50b28f113c5
Kenji Okamoto <kokamot1@my.westga.edu>
2016-03-04 10:20:22 -0800

    Implement calculate button handler to get the end balance from user input

--- app/src/main/java/edu/westga/kenjiokamotoinvestmentcalculator/MainActivity.java
+++ app/src/main/java/edu/westga/kenjiokamotoinvestmentcalculator/MainActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import edu.westga.kenjiokamotoinvestmentcalculator.model.InvestmentCalculator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+    public void calculateButtonClick(View v) {
+        EditText paymentEditText = (EditText) findViewById(R.id.paymentEditText);
+        EditText rateEditText = (EditText) findViewById(R.id.rateEditText);
+        EditText yearsEditText = (EditText) findViewById(R.id.yearsEditText);
+        TextView resultTextView = (TextView) findViewById(R.id.endBalancetextView);
+        double payment;
+        double rate;
+        int years;
+
+        try {
+            payment = Double.parseDouble(paymentEditText.getText().toString());
+            rate = Double.parseDouble(rateEditText.getText().toString());
+        } catch (NumberFormatException exception) {
+            resultTextView.setText("Payment and Rate need to be decimal numbers");
+            return;
+        }
+        try {
+            years = Integer.parseInt(yearsEditText.getText().toString());
+        } catch (NumberFormatException exception) {
+            resultTextView.setText("Years must be an integer");
+            return;
+        }
+        resultTextView.setText(InvestmentCalculator.getFutureValue(payment, rate, years));
 
     }
 
